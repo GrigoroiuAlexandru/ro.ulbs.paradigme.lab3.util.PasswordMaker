070d7d7a0a05914c9d81e205049a9458a71c0fef
GrigoroiuAlexandru <gheorghealexandru.grigoroiu@ulbsibiu.ro>
2025-03-19 21:20:58 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,39 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+
+import java.util.Random;
+
+class StringRandomizer {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final Random RANDOM = new Random();
+
+    public static String generateRandomString(int length) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
         }
+        return sb.toString();
+    }
+}
+
+    class PasswordMaker {
+    private static final Random RANDOM = new Random();
+    private static final int MAGIC_NUMBER = RANDOM.nextInt(6) + 5; // între 5 și 10
+    private static final String magicString = StringRandomizer.generateRandomString(20);
+
+    public PasswordMaker() {}
+
+    public String getPassword() {
+        String randomPart = StringRandomizer.generateRandomString(MAGIC_NUMBER);
+        String magicPart = StringRandomizer.generateRandomString(10);
+        String lengthPart = String.valueOf(magicString.length());
+        int randomInt = RANDOM.nextInt(51);
+
+        return randomPart + magicPart + lengthPart + randomInt;
+    }
+
+    public static void main(String[] args) {
+        PasswordMaker parola = new PasswordMaker();
+        System.out.println("Generated Password: " + parola.getPassword());
     }
 }
